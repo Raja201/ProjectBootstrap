d910d5d51430fad0950383c417300591c5ceb262
Raja Sahu <rajasahu2246@gmail.com>
2022-06-25 10:47:56 +0530

    second commit

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(function(){
+    
+    $('#reserve-button').click(function (){
+      $('#reserveModal').modal('toggle');
+    });
+  
+    $('#login-Btn').click(function (){
+      $('#loginModal').modal('toggle');
+    });
+  
+    $("#carousel-button").click(function(){
+      if ($("#carousel-button").children("span").hasClass('fa-pause')) 
+          {
+          $("#mycarousel").carousel('pause');
+          $("#carousel-button").children("span").removeClass('fa-pause');
+          $("#carousel-button").children("span").addClass('fa-play');
+      }
+      else if ($("#carousel-button").children("span").hasClass('fa-play')){
+          $("#mycarousel").carousel('cycle');
+          $("#carousel-button").children("span").removeClass('fa-play');
+          $("#carousel-button").children("span").addClass('fa-pause'); 
+      }
+    });
+  
+  });
